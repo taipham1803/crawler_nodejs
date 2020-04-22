72bcb28aa336b9c2f6cf80edab5d81b6a28102aa
TaiPham <taipham1803@gmail.com>
2020-04-23 01:47:26 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const puppeteer = require('puppeteer');
+
+let electronicUrl = 'https://nshopvn.com/';
+(async () => {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto(electronicUrl);
+
+  let electronicData = await page.evaluate(() => {
+    let products = [];
+    let product_wrapper = document.querySelectorAll('.product');
+    product_wrapper.forEach((product) => {
+      let dataJson = {};
+      try {
+        dataJson.img = product.querySelector('.image > img').src;
+        dataJson.title = product.querySelector('.product-title').innerText;
+        dataJson.price = product.querySelector('.price').innerText;
+      }
+      catch (err) {
+          console.log(err)
+      }
+      products.push(dataJson);
+    });
+    return products;
+  });
+
+   console.log(electronicData);
+   await browser.close();
+})();
